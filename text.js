082b5f829f146457711e89430c6bbfcba2c9c8fe
inaralmeida <inaral.ralmeida@gmail.com>
2023-06-04 00:45:38 -0300

    feat: header

---
+++ text.js
@@ -0,0 +1,51 @@
+export const text = [
+  //header
+  'Um histórico de tudo o que você copia', //destaque
+  'Clipboard permite rastrear e organizar tudo o que você copia.Acesse instantaneamente suClipboard em todos os seus dispositivos', //descrição
+  ' Baixe para iOS', //btn
+  'Baixe para Mac', //btn
+
+  //section um
+  'Acompanhe seus seus snippets', //destaque
+  'Clipboard armazena instantaneamente qualquer item que você copia na nuvem, o que significa que você pode acessar seus snippets imediatamente em todos os seus dispositivos.Nossos aplicativos para Mac e iOS ajudarão você organizar tudo. ', //descrição
+
+  //lista vertical
+  'Pesquisa rápida', //titulo
+  ' Pesquise facilmente seus snippets por conteúdo, categoria, endereço da web,aplicativo e muito mais.', //descrição
+  'iCloud Sync', //titulo
+  'salva e sincroniza instantaneamente trechos em todos os seus dispositivos.', //descricao
+  'Histórico completo', //titulo
+  'Recupere todos os trechos desde o primeiro momento em que você começou a usar o aplicativo.', //descricao
+
+  //section dois
+  'Acesso Área de transferência em qualquer lugar', //titulo
+  'Esteja você em trânsito ou em seu computador, você pode acessar todos os seus texto do Clipboard com apenas alguns cliques.', //descrição
+
+  //section tres
+  'Turbine seu fluxo de trabalho ', //titulo
+  'Temos as ferramentas para aumentar a sua produtividade.', //descricao
+
+  //lista horizontal
+  'Crie listas', //titulo
+  'Garanta que informações confidenciais nunca cheguem sua area de transferencia excluindo certas fontes.', //descricao
+
+  'Trechos de texto simples', //titulo
+  'Remova a formatação indesejada de texto copiado para uma aparência consistente.', //descricao
+
+  'Visualização rápida', //titulo
+  'Pré-visualização rápida de todos os fragmentos do Clipboard para fácil acesso', //descricao
+
+  //section quatro
+  'Área de transferência para iOS e Mac OS', //titulo
+  'Disponível gratuitamente na App Store. Baixar para Mac ou iOS, sincronize com o iCloud e você está pronto para começar a adicionar ao seu Clipboard.', //descricao
+
+  'Baixar para Mac', //btn
+  'Baixar para iOS', //btn
+
+  //footer
+  'Perguntas frequentes',
+  ' Fale conosco',
+  'Política de privacidade',
+  'Kit de imprensa',
+  'Guia de instalação',
+]
